968933eaa3e40188ffc1fa9af6d514dce0103dbb
miley yao <32486958+mileyyao@users.noreply.github.com>
2021-08-08 11:20:41 -0400

    Update runner_gamma.py

--- runner_gamma.py
+++ runner_gamma.py
@@ -8,10 +8,9 @@ import matplotlib.pyplot as plt
 
 
 W = np.array(pd.read_csv('synthetic_gamma.csv'))
-X = loadmat('X_hawkes_syn_gamma.mat').get('X')
+X = loadmat('X_hawkes_syn_gamma.mat').get('X') # mat file dimension: #. student x #. time stamps x #. assignments; e.g. X[i,:,j]: sequence of student-assignment pair (s_i, a_j)
 itermax = 30
-W_learn, funcVal, M_learn, U_learn = clusterhawkes(X, 15, 0.1, 3, 0.7, itermax, bFlag= 2,prior = 'gamma',gamma_theta=[1/5,1/5,1/5],gamma_k= [2,3,4])
-pd.DataFrame(M_learn).to_csv('synthetic_learnedM_gamma_iter'+str(itermax)+'.csv',index = False)
+W_learn, funcVal, M_learn, U_learn = clusterhawkes(X, 15, 0.1, 3, 0.7, itermax, bFlag= 2,prior = 'gamma',gamma_theta=[1/5,1/5,1/5],gamma_k= [2,3,4]) # learned parms
 clus_num = 3
 
 clus_task_num = 30
@@ -26,9 +25,6 @@ for i in range(1, clus_num + 1):
     clusModel = W_learn[:, [x - 1 for x in range(i, task_num + 1, clus_num)]]
     OrderedLearnModel[:, range((i - 1) * clus_task_num, i * clus_task_num)] = clusModel
 corr = 1 - np.corrcoef(OrderedLearnModel.T)
-
-pd.DataFrame(W_learn).to_csv('synthetic_learnedW_gamma_iter'+str(itermax)+'.csv',index = False)
-pd.DataFrame(U_learn).to_csv('synthetic_learnedU_gamma_iter'+str(itermax)+'.csv',index = False)
 plt.imshow(corr, cmap=cm.gray)
 plt.title('learned model without gamma prior')
 plt.show()
